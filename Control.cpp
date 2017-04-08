609fd7155b75c4e02f6625cf9246cfdb3747dfc8
Eric Cherba <echerba@gmail.com>
2017-04-08 09:08:55 -0700

    Initial checkin of the controls, this will read pins 8 and 9 and figure out if we should pause the animation, or move to the next animation.

---
+++ Control.cpp
@@ -0,0 +1,11 @@
+#ifndef __INC_CONTROL_H
+#define __INC_CONTROL_H
+#include "Control.h"
+
+void setup_controls()
+{
+    pinMode(PlayPin, INPUT);
+    pinMode(NextPin, INPUT);
+}
+
+#endif __INC_CONTROL_H
---
+++ Control.h
@@ -0,0 +1,18 @@
+
+const int PlayPin = 8
+const int NextPin = 9
+
+bool play = FALSE;
+int playButtonState;             // the current reading from the input pin
+int lastPlayButtonState = LOW;   // the previous reading from the input pin
+int nextButtonState;             // the current reading from the input pin
+int lastNextButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup_controls();
+
+void ReadControls(bool&play, bool&toggleNext);
